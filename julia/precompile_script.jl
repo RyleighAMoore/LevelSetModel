dbbb621c9d3de3f48da5ec14e43819549b4dfbdf
Brian Chapman <brian.chapman@utah.edu>
2018-11-27 00:28:27 -0700

    distributed progressing

---
+++ julia/precompile_script.jl
@@ -0,0 +1,15 @@
+using Distributed
+using Plots
+using JSON
+using UUIDs
+using Dates
+using SQLite
+using Formatting
+using Images
+using FileIO
+using PyPlot
+using DistributedArrays
+using BenchmarkTools
+using SymPy
+using ImageMagick
+pyplot()
